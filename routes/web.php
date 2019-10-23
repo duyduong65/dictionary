4d9afdb7c14c92a8687c60f8dd2c0a5922b0beda
duyduong65 <tg.bluesky66@gmail.com>
2019-10-23 14:16:33 +0700

    final

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dictionary', function (){
+    return view("show_dictionary");
+});
+Route::post('/dictionary', "dictionaryController@translate")->name('dictionary');
+
